22e5645f68cda2a321444ef2fbca30fd2f205100
dynamitesushi <winson.hero@gmail.com>
2023-06-13 15:14:34 +0100

    added Android.showTitleBar

--- src/components/TitleBar/index.tsx
+++ src/components/TitleBar/index.tsx
@@ -3,6 +3,7 @@ import logo from '../../assets/logo.svg';
 import pace from '../../assets/pace.svg';
 import backArrow from '../../assets/arrow_back.svg';
 import { useNavigate } from 'react-router-dom';
+import useAndroidShowTitleBar from "../../hooks/useAndroidShowTitleBar";
 
 type TitleBarProps = {
   home?: boolean;
@@ -12,8 +13,11 @@ type TitleBarProps = {
 
 const TitleBar: React.FC<TitleBarProps> = ({ home, back, showPendingTransaction }) => {
   const navigate = useNavigate();
+  const openTitleBar = useAndroidShowTitleBar();
 
-  const goBack = () => {
+  const goBack = (e: React.MouseEvent<any>) => {
+    e.stopPropagation();
+
     if (back) {
       return back();
     }
@@ -21,8 +25,16 @@ const TitleBar: React.FC<TitleBarProps> = ({ home, back, showPendingTransaction 
     navigate(-1);
   };
 
+  const displayPendingTransaction = (e: React.MouseEvent<any>) => {
+    e.stopPropagation();
+
+    if (showPendingTransaction) {
+      showPendingTransaction();
+    }
+  }
+
   return (
-    <div className="title-bar bg-main flex items-center px-4">
+    <div className="title-bar bg-main flex items-center px-4" onClick={openTitleBar}>
       <div className="grid grid-cols-12 w-full">
         <div className="col-span-6 flex items-center">
           {home && <img src={logo} alt="Maximise logo" className="mt-2" />}
@@ -30,7 +42,7 @@ const TitleBar: React.FC<TitleBarProps> = ({ home, back, showPendingTransaction 
         </div>
         <div className="col-span-6 flex items-center justify-end">
           {home && showPendingTransaction && (
-            <div onClick={showPendingTransaction} className="cursor-pointer pr-1">
+            <div onClick={displayPendingTransaction} className="cursor-pointer pr-1">
               <img src={pace} alt="pending transaction" className="cursor-pointer" />
             </div>
           )}
